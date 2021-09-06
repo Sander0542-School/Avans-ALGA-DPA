bf04c60ea9177bcf9d1e624f71ec850293af592d
Sander Jochems <sanderjochems@hotmail.nl>
2021-09-06 14:51:18 +0200

    Create Fold.cs

---
+++ src/Avans.FlatGalaxy.Persistence/CelestialBodies/Fold.cs
@@ -0,0 +1,9 @@
+﻿namespace Avans.FlatGalaxy.Persistence.CelestialBodies
+{
+    public class Fold
+    {
+        private CelestialBody Body1 { get; set; }
+
+        private CelestialBody Body2 { get; set; }
+    }
+}
