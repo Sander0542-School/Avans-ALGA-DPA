5cfee94cec42fd417cdbd5315dd166f7b6498c30
Tommy Hosewol <t.hosewol@gmail.com>
2021-09-08 11:44:59 +0200

    Finish XML loader

--- src/Avans.FlatGalaxy.Persistence/Loaders/Configuration/XmlConfigurationLoader.cs
+++ src/Avans.FlatGalaxy.Persistence/Loaders/Configuration/XmlConfigurationLoader.cs
@@ -1,7 +1,5 @@
-﻿using System.Drawing;
+﻿
 using System.Xml;
-using Avans.FlatGalaxy.Persistence.CelestialBodies;
-using Avans.FlatGalaxy.Persistence.Factories;
 using Avans.FlatGalaxy.Persistence.Factories.Common;
 
 namespace Avans.FlatGalaxy.Persistence.Loaders.Configuration
@@ -19,11 +17,39 @@ namespace Avans.FlatGalaxy.Persistence.Loaders.Configuration
             var xmlBody = new XmlDocument();
             xmlBody.LoadXml(content);
 
-            foreach (XmlNode xmlNode in xmlBody.ChildNodes)
+            var xmlNode = xmlBody.ChildNodes[1];
+            if (xmlNode == null) return galaxy;
+
+            foreach (XmlNode celestialBody in xmlNode.ChildNodes)
             {
-                if (xmlNode.Name == "planet")
+                if (celestialBody.NodeType == XmlNodeType.Element)
                 {
-                    // var planet = CelestialBodyFactory.Create();
+                    var name = ((XmlText) celestialBody["name"]?.ChildNodes[0])?.Data;
+                    var type = celestialBody.Name;
+                    var color = ((XmlText) celestialBody["color"]?.ChildNodes[0])?.Data;
+                    var onCollision = ((XmlText) celestialBody["oncollision"]?.ChildNodes[0])?.Data;
+
+                    var positionNode = (XmlNode) celestialBody["position"];
+                    var posX = 0;
+                    var posY = 0;
+                    var radius = 0;
+                    if (positionNode != null)
+                    {
+                        posX = int.Parse(((XmlText) positionNode["x"]?.ChildNodes[0])?.Data ?? "0");
+                        posY = int.Parse(((XmlText) positionNode["y"]?.ChildNodes[0])?.Data ?? "0");
+                        radius = int.Parse(((XmlText) positionNode["radius"]?.ChildNodes[0])?.Data ?? "0");
+                    }
+
+                    var speedNode = (XmlNode) celestialBody["position"];
+                    var speedX = 0;
+                    var speedY = 0;
+                    if (speedNode != null)
+                    {
+                        speedX = int.Parse(((XmlText) speedNode["x"]?.ChildNodes[0])?.Data ?? "0");
+                        speedY =  int.Parse(((XmlText) speedNode["y"]?.ChildNodes[0])?.Data ?? "0");
+                    }
+
+                    galaxy.CelestialBodies.Add(CelestialBodyFactory.Create(name, type, posX, posY, speedX, speedY, radius, color, onCollision));
                 }
             }
 
