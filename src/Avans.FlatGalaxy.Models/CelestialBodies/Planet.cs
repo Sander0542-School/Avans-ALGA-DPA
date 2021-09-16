9a7feb9c4f17238f73400661c2f994141a3bb9c1
Sander Jochems <sanderjochems@hotmail.nl>
2021-09-16 16:31:53 +0200

    Remove NamedCelestialBody

--- src/Avans.FlatGalaxy.Models/CelestialBodies/Planet.cs
+++ src/Avans.FlatGalaxy.Models/CelestialBodies/Planet.cs
@@ -3,10 +3,13 @@ using Avans.FlatGalaxy.Models.CelestialBodies.States;
 
 namespace Avans.FlatGalaxy.Models.CelestialBodies
 {
-    public class Planet : NamedCelestialBody
+    public class Planet : CelestialBody
     {
-        public Planet(string name, int x, int y, double vx, double vy, int radius, Color color, ICollisionState collisionState) : base(name, x, y, vx, vy, radius, color, collisionState)
+        public string Name { get; private set; }
+
+        public Planet(string name, int x, int y, double vx, double vy, int radius, Color color, ICollisionState collisionState) : base(x, y, vx, vy, radius, color, collisionState)
         {
+            Name = name;
         }
     }
 }
