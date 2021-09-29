ce2a0703a6e4250ddc17afa6cd1695ca3cc570f5
Tommy Hosewol <t.hosewol@gmail.com>
2021-09-29 14:15:47 +0200

    Delete Fold.cs

--- src/Avans.FlatGalaxy.Models/CelestialBodies/Fold.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Avans.FlatGalaxy.Models.CelestialBodies
-{
-    public class Fold
-    {
-        private CelestialBody Body1 { get; set; }
-
-        private CelestialBody Body2 { get; set; }
-    }
-}
