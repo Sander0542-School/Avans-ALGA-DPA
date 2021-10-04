9309e12371fd8a7e3bbf174aa816387af701d344
Tommy Hosewol <t.hosewol@gmail.com>
2021-10-04 18:21:13 +0200

    Update CheapestPathFinder.cs

--- src/Avans.FlatGalaxy.Simulation/PathFinding/CheapestPathFinder.cs
+++ src/Avans.FlatGalaxy.Simulation/PathFinding/CheapestPathFinder.cs
@@ -3,40 +3,11 @@ using Avans.FlatGalaxy.Models.CelestialBodies;
 
 namespace Avans.FlatGalaxy.Simulation.PathFinding
 {
-    public class CheapestPathFinder : IPathFinder
+    public class CheapestPathFinder : PathFinder
     {
-        public List<Planet> Calculate(Planet start, Planet end)
+        public override List<Planet> Find(Planet start, Planet end)
         {
-            // var previous = new Dictionary<Planet, Planet>();
-            // var queue = new Queue<Planet>();
-            //
-            // queue.Enqueue(start);
-            //
-            // while (queue.Count > 0)
-            // {
-            //     var planet = queue.Dequeue();
-            //     
-            //     foreach (var neighbour in planet.Neighbours)
-            //     {
-            //         if (previous.ContainsKey(neighbour)) continue;
-            //         
-            //         previous[neighbour] = planet;
-            //         queue.Enqueue(neighbour);
-            //     }
-            // }
-            //
-            // var shortest = new List<Planet>();
-            // var current = end;
-            // while (!current.Equals(start))
-            // {
-            //     shortest.Add(current);
-            //     current = previous[current];
-            // }
-            //
-            // shortest.Add(start);
-            // shortest.Reverse();
-            //
-            // return shortest;
+            return new();
         }
     }
 }
