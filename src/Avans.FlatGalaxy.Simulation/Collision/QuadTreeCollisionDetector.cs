04b970ea194306780c163844ff4ae1a3a93c5e38
Sander Jochems <sanderjochems@hotmail.nl>
2021-10-04 14:00:14 +0200

    Create QuadTreeCollisionDetector.cs

---
+++ src/Avans.FlatGalaxy.Simulation/Collision/QuadTreeCollisionDetector.cs
@@ -0,0 +1,21 @@
+﻿using Avans.FlatGalaxy.Models;
+using Avans.FlatGalaxy.Simulation.Data;
+using Avans.FlatGalaxy.Simulation.Extensions;
+
+namespace Avans.FlatGalaxy.Simulation.Collision
+{
+    public class QuadTreeCollisionDetector : CollisionDetector
+    {
+        protected override void Collide(Galaxy galaxy)
+        {
+            var quadTree = new QuadTree(new Bounds(0, 0, ISimulator.Height, ISimulator.Width));
+
+            foreach (var celestialBody in galaxy.CelestialBodies)
+            {
+                quadTree.Insert(celestialBody);
+            }
+
+            quadTree.Collisions(this);
+        }
+    }
+}
