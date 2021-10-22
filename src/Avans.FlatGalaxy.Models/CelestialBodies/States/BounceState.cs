553a698bf5e3de2f0592ffeaed8299c6c328d170
Sander Jochems <sanderjochems@hotmail.nl>
2021-10-22 17:06:11 +0200

    Update BounceState.cs

--- src/Avans.FlatGalaxy.Models/CelestialBodies/States/BounceState.cs
+++ src/Avans.FlatGalaxy.Models/CelestialBodies/States/BounceState.cs
@@ -2,10 +2,17 @@
 {
     public class BounceState : ICollisionState
     {
+        private int _collisions;
+        
         public void Collide(CelestialBody self, CelestialBody other)
         {
             self.VX = -self.VX;
             self.VY = -self.VY;
+
+            if (++_collisions == 5)
+            {
+                self.CollisionState = new BlinkState();
+            }
         }
     }
 }
