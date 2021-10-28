7191e26ea61ea96370b38ad874233b94d9059b70
Sander Jochems <sanderjochems@hotmail.nl>
2021-10-28 12:06:28 +0200

    Create Commands

---
+++ src/Avans.FlatGalaxy.Simulation/Commands/SpeedDownCommand.cs
@@ -0,0 +1,12 @@
+﻿using Avans.FlatGalaxy.Simulation.Commands.Common;
+
+namespace Avans.FlatGalaxy.Simulation.Commands
+{
+    public class SpeedDownCommand : ICommand
+    {
+        public void Execute(ISimulator simulator)
+        {
+            simulator.SpeedDown(ISimulator.SpeedDiff);
+        }
+    }
+}
