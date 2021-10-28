40b4fe0e7c89a1ade8b3658d310fe53e1cebdb26
Sander Jochems <sanderjochems@hotmail.nl>
2021-10-28 15:29:12 +0200

    Update GalaxyMemento.cs

--- src/Avans.FlatGalaxy.Simulation/Bookmark/GalaxyMemento.cs
+++ src/Avans.FlatGalaxy.Simulation/Bookmark/GalaxyMemento.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Avans.FlatGalaxy.Models;
 using Avans.FlatGalaxy.Models.CelestialBodies;
@@ -8,16 +9,19 @@ namespace Avans.FlatGalaxy.Simulation.Bookmark
 {
     public class GalaxyMemento : IMemento<Galaxy>
     {
-        private readonly IList<CelestialBody> _celestialBodies;
+        private readonly IDictionary<CelestialBody, string[]> _celestialBodies;
 
         public GalaxyMemento(Galaxy galaxy)
         {
-            _celestialBodies = galaxy.CelestialBodies.Select(body => body.Clone()).ToList();
+            _celestialBodies = galaxy.CelestialBodies.ToDictionary(body => body.Clone(), body => body is Planet planet ? planet.Neighbours.Select(planet1 => planet1.Name).ToArray() : Array.Empty<string>());
         }
 
         public Galaxy GetState()
         {
-            return new Galaxy(_celestialBodies);
+            var galaxy = new Galaxy(_celestialBodies.Keys);
+            galaxy.MapNeighbours(_celestialBodies.Where(pair => pair.Key is Planet).ToDictionary(pair => pair.Key as Planet, pair => pair.Value));
+
+            return galaxy;
         }
     }
 }
